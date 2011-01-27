3fbe1c9761ab2df5bac26c4725305ebc80b5e7d9
asual <rostislav@asual.com>
2011-01-27 14:26:38 +0200

    Refactoring and improvements.

--- src/main/java/com/asual/lesscss/LessException.java
+++ src/main/java/com/asual/lesscss/LessException.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,69 +21,62 @@ import java.util.List;
 
 /**
  * @author Rostislav Hristov
+ * @author Uriah Carpenter
  */
 public class LessException extends Exception {
 
-    private static final long serialVersionUID = 662552833197468936L;
+	private static final long serialVersionUID = 662552833197468936L;
 
 	private String errorType;
 	private String filename;
 	private int line;
 	private int column;
 	private List<String> extract;
-    
-	public static LessException fromLessJsError(String errorType, String message, String filename, int line, int column, List<String> extract) {
-		LessException le = new LessException(message);
-		if (errorType == null) {
-			le.errorType = "LESSJS Error";
-		}
-		else {
-			le.errorType = errorType;
-		}
-		le.filename = filename;
-		le.line = line;
-		le.column = column;
-		le.extract = extract;
-		return le;
+	
+	public LessException() {
+		super();
 	}
 	
-    public LessException() {
-        super();
-    }
-    
-    public LessException(String message) {
-        super(message);
-    }
-    
-    public LessException(String message, Throwable e) {
-        super(message, e);
-    }
+	public LessException(String message) {
+		super(message);
+	}
+	
+	public LessException(String message, Throwable e) {
+		super(message, e);
+	}
+	
+	public LessException(String message, String errorType, String filename, int line, int column, List<String> extract) {
+		super(message);
+		this.errorType = errorType != null ? errorType : "LESS Error";
+		this.filename = filename;
+		this.line = line;
+		this.column = column;
+		this.extract = extract;
+	}
+	
+	public LessException(Throwable e) {
+		super(e);
+	}
+	
+	@Override
+	public String getMessage() {
+		if (errorType != null) {
+			String msg = String.format("%s: %s (line %s, column %s)", errorType, super.getMessage(), line, column);
+			if (!(extract == null) && !extract.isEmpty()) {
+				msg += " near";
+				for (String l : extract) {
+					msg += "\n" + l;
+				}
+			}
+			return msg;
+		}
+		
+		return super.getMessage();
+	}
 
-    public LessException(Throwable e) {
-        super(e);
-    }
-    
-    @Override
-    public String getMessage()
-    {
-    	if (errorType != null) {
-    		String msg = String.format("%s: %s (line %s, column %s)", errorType, super.getMessage(), line, column);
-    		if (!(extract == null) && !extract.isEmpty())
-    		{
-    			msg += " near";
-    			for (String l : extract) {
-    				msg += "\n" + l;
-    			}
-    		}
-    		return msg;
-    	}
-    	
-    	return super.getMessage();
-    }
-
-    /**
-     * Type of error as reported by less.js
-     */
+	/**
+	 * Type of error as reported by less.js
+	 */
 	public String getErrorType() {
 		return errorType;
 	}
@@ -115,5 +108,5 @@ public class LessException extends Exception {
 	public List<String> getExtract() {
 		return extract;
 	}
-    
+	
 }
--- src/test/java/com/asual/lesscss/LessEngineTest.java
+++ src/test/java/com/asual/lesscss/LessEngineTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,120 +27,109 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author Rostislav Hristov
+ * @author Uriah Carpenter
+ * @author Eliot Sykes
  */
 public class LessEngineTest {
-    
-    private static LessEngine engine;
-    
-    @BeforeClass
-    public static void before() {
-        engine = new LessEngine();
-    }
-    
-    @Test
-    public void parse() throws LessException {
-        assertEquals("div {\n  width: 2;\n}\n", engine.compile("div { width: 1 + 1 }"));
-    }
-    
-    @Test
-    public void compileToString() throws LessException, IOException {
-        assertEquals("body {\n  color: #f0f0f0;\n}\n", 
-                engine.compile(loadResource("test.css")));
-    }
-    
-    @Test
-    public void compileToFile() throws LessException, IOException {
-        File tempDir = new File(System.getProperty("java.io.tmpdir"));
-        File tempFile = File.createTempFile("less.css", null, tempDir);
-        engine.compile(
-        		new File(loadResource("test.css").getPath()),
-        		new File(tempFile.getAbsolutePath()));
-        FileInputStream fstream = new FileInputStream(tempFile.getAbsolutePath());
-        DataInputStream in = new DataInputStream(fstream);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String strLine;
-        StringBuilder sb = new StringBuilder();
-        while ((strLine = br.readLine()) != null) {
-            sb.append(strLine);
-        }
-        in.close();
-        assertEquals("body {  color: #f0f0f0;}", sb.toString());
-        tempFile.delete();
-    }
+	
+	private static LessEngine engine;
+	
+	@BeforeClass
+	public static void before() {
+		engine = new LessEngine();
+	}
+	
+	@Test
+	public void parse() throws LessException {
+		assertEquals("div {\n  width: 2;\n}\n", engine.compile("div { width: 1 + 1 }"));
+	}
+	
+	@Test
+	public void compileToString() throws LessException, IOException {
+		assertEquals("body {\n  color: #f0f0f0;\n}\n", 
+				engine.compile(getUrl("test.css")));
+	}
+	
+	@Test
+	public void compileToFile() throws LessException, IOException {
+		File tempDir = new File(System.getProperty("java.io.tmpdir"));
+		File tempFile = File.createTempFile("less.css", null, tempDir);
+		engine.compile(
+				new File(getUrl("test.css").getPath()),
+				new File(tempFile.getAbsolutePath()));
+		FileInputStream fstream = new FileInputStream(tempFile.getAbsolutePath());
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		StringBuilder sb = new StringBuilder();
+		while ((strLine = br.readLine()) != null) {
+			sb.append(strLine);
+		}
+		in.close();
+		assertEquals("body {  color: #f0f0f0;}", sb.toString());
+		tempFile.delete();
+	}
 
-    @Test
-    public void compileToStringForMultipleImports() throws LessException, IOException {
-        String expected = "body {\n" +
-                "  font-family: Arial, Helvetica;\n" +
-                "}\n" +
-                "body {\n" +
-                "  width: 960px;\n" +
-                "  margin: 0;\n" +
-                "}\n" +
-                "#header {\n" +
-                "  border-radius: 5px;\n" +
-                "  -webkit-border-radius: 5px;\n" +
-                "  -moz-border-radius: 5px;\n" +
-                "}\n" +
-                "#footer {\n" +
-                "  border-radius: 10px;\n" +
-                "  -webkit-border-radius: 10px;\n" +
-                "  -moz-border-radius: 10px;\n" +
-                "}\n";
-        String actual = engine.compile(loadResource("multiple-imports.css"));
-        assertEquals(expected, actual);
-    }
-
-    private URL loadResource(String filename) {
-        return getClass().getClassLoader().getResource("META-INF/" + filename);
-    }
-    
-    @Test(expected = LessException.class)
-    public void testUndefinedErrorInput() throws IOException, LessException {
-    	String input = IOUtils.toString(getClass().getClassLoader().getResource("META-INF/undefined-error.css").openStream());
-    	
-    	try {
-			engine.compile(input);
-    	}
-    	catch (LessException e) {
-    		assertTrue("is undefined error", e.getMessage().contains("Error: .bgColor is undefined (line 2, column 4)"));
-    		throw e;
-    	}
-    	
-    }
+	@Test
+	public void compileToStringForMultipleImports() throws LessException, IOException {
+		String expected = "body {\n" +
+				"  font-family: Arial, Helvetica;\n" +
+				"}\n" +
+				"body {\n" +
+				"  width: 960px;\n" +
+				"  margin: 0;\n" +
+				"}\n" +
+				"#header {\n" +
+				"  border-radius: 5px;\n" +
+				"  -webkit-border-radius: 5px;\n" +
+				"  -moz-border-radius: 5px;\n" +
+				"}\n" +
+				"#footer {\n" +
+				"  border-radius: 10px;\n" +
+				"  -webkit-border-radius: 10px;\n" +
+				"  -moz-border-radius: 10px;\n" +
+				"}\n";
+		assertEquals(expected, engine.compile(getUrl("multiple-imports.css")));
+	}
+	
+	@Test(expected = LessException.class)
+	public void testUndefinedErrorInput() throws IOException, LessException {
+		try {
+			engine.compile(getUrl("undefined-error.css"));
+		} catch (LessException e) {
+			assertTrue("is undefined error", e.getMessage().contains("Error: .bgColor is undefined (line 2, column 4)"));
+			throw e;
+		}
+		
+	}
  
-    @Test(expected = LessException.class)
-    public void testSyntaxErrorInput() throws IOException, LessException
-    {
-    	String input = IOUtils.toString(getClass().getClassLoader().getResource("META-INF/syntax-error.css").openStream());
-    	
-    	try {
-			engine.compile(input);
-    	}
-    	catch (LessException e) {
-    		assertTrue("is syntax error", e.getMessage().contains("Syntax Error: Missing closing `}` (line -1, column -1)"));
-    		throw e;
-    	}
-    }
-    
-    @Test(expected = LessException.class)
-    public void testParseErrorInput() throws IOException, LessException
-    {
-    	String input = IOUtils.toString(getClass().getClassLoader().getResource("META-INF/parse-error.css").openStream());
-    	
-    	try {
-			engine.compile(input);
-    	}
-    	catch (LessException e) {
-    		assertTrue("is parse error", e.getMessage().contains("Parse Error: Syntax Error on line 2"));
-    		throw e;
-    	}
-    	
-    }
+	@Test(expected = LessException.class)
+	public void testSyntaxErrorInput() throws IOException, LessException {
+		try {
+			engine.compile(getUrl("syntax-error.css"));
+		} catch (LessException e) {
+			assertTrue("is syntax error", e.getMessage().contains("Syntax Error: Missing closing `}` (line -1, column -1)"));
+			throw e;
+		}
+	}
+	
+	@Test(expected = LessException.class)
+	public void testParseErrorInput() throws IOException, LessException {
+		try {
+			engine.compile(getUrl("parse-error.css"));
+		} catch (LessException e) {
+			assertTrue("is parse error", e.getMessage().contains("Parse Error: Syntax Error on line 2"));
+			throw e;
+		}
+	}
+	
+	private URL getUrl(String filename) {
+		return getClass().getClassLoader().getResource("META-INF/" + filename);
+	}
+
 }
