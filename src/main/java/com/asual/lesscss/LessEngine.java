844deb593804d067140528b0ddad7d6db50044c6
asual <rostislav@asual.com>
2011-01-27 14:25:49 +0200

    Improves formatting.

--- src/main/java/com/asual/lesscss/LessEngine.java
+++ src/main/java/com/asual/lesscss/LessEngine.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -37,154 +37,150 @@ import org.mozilla.javascript.tools.shell.Global;
 
 /**
  * @author Rostislav Hristov
+ * @author Uriah Carpenter
  */
 public class LessEngine {
 
-    private final Log logger = LogFactory.getLog(getClass());
+	private final Log logger = LogFactory.getLog(getClass());
 
-    private Scriptable scope;
-    private Function cs;
-    private Function cf;
-    
-    public LessEngine() {
-    	try {
-        	logger.debug("Initializing LESS Engine.");
-        	URL browser = getClass().getClassLoader().getResource("META-INF/browser.js");
-        	URL less = getClass().getClassLoader().getResource("META-INF/less.js");
-        	URL engine = getClass().getClassLoader().getResource("META-INF/engine.js");
-        	Context cx = Context.enter();
-        	cx.setOptimizationLevel(9);
-        	Global global = new Global();
-            global.init(cx);          
-            scope = cx.initStandardObjects(global);
-    		cx.evaluateReader(scope, new InputStreamReader(browser.openConnection().getInputStream()), browser.getFile(), 1, null);
-    		cx.evaluateReader(scope, new InputStreamReader(less.openConnection().getInputStream()), less.getFile(), 1, null);
-    		cx.evaluateReader(scope, new InputStreamReader(engine.openConnection().getInputStream()), engine.getFile(), 1, null);
-    		cs = (Function) scope.get("compileString", scope);
-    		cf = (Function) scope.get("compileFile", scope);
-    		Context.exit();
+	private Scriptable scope;
+	private Function cs;
+	private Function cf;
+	
+	public LessEngine() {
+		try {
+			logger.debug("Initializing LESS Engine.");
+			URL browser = getClass().getClassLoader().getResource("META-INF/browser.js");
+			URL less = getClass().getClassLoader().getResource("META-INF/less.js");
+			URL engine = getClass().getClassLoader().getResource("META-INF/engine.js");
+			Context cx = Context.enter();
+			cx.setOptimizationLevel(9);
+			Global global = new Global();
+			global.init(cx);		  
+			scope = cx.initStandardObjects(global);
+			cx.evaluateReader(scope, new InputStreamReader(browser.openConnection().getInputStream()), browser.getFile(), 1, null);
+			cx.evaluateReader(scope, new InputStreamReader(less.openConnection().getInputStream()), less.getFile(), 1, null);
+			cx.evaluateReader(scope, new InputStreamReader(engine.openConnection().getInputStream()), engine.getFile(), 1, null);
+			cs = (Function) scope.get("compileString", scope);
+			cf = (Function) scope.get("compileFile", scope);
+			Context.exit();
 		} catch (Exception e) {
 			logger.error("LESS Engine intialization failed.", e);
 		}
-    }
-    
-    public String compile(String input) throws LessException {
-        try {
-        	long time = System.currentTimeMillis();
-            String result = call(cs, new Object[] {input});
-            logger.debug("The compilation of '" + input + "' took " + (System.currentTimeMillis () - time) + " ms.");
-            return result;
+	}
+	
+	public String compile(String input) throws LessException {
+		try {
+			long time = System.currentTimeMillis();
+			String result = call(cs, new Object[] {input});
+			logger.debug("The compilation of '" + input + "' took " + (System.currentTimeMillis () - time) + " ms.");
+			return result;
 		} catch (Exception e) {
-			throw parseJSException(e);
+			throw parseLessException(e);
 		}
-    }
-    
-    public String compile(URL input) throws LessException {
-    	try {
-            long time = System.currentTimeMillis();
-            String result = call(cf, new Object[] {input.getProtocol() + ":" + input.getFile(), getClass().getClassLoader()});
-            logger.debug("The compilation of '" + input + "' took " + (System.currentTimeMillis () - time) + " ms.");
-            return result;
+	}
+	
+	public String compile(URL input) throws LessException {
+		try {
+			long time = System.currentTimeMillis();
+			String result = call(cf, new Object[] {input.getProtocol() + ":" + input.getFile(), getClass().getClassLoader()});
+			logger.debug("The compilation of '" + input + "' took " + (System.currentTimeMillis () - time) + " ms.");
+			return result;
 		} catch (Exception e) {
-			throw parseJSException(e);
+			throw parseLessException(e);
 		}
-    }
-    
-    public String compile(File input) throws LessException {
-    	try {
-            long time = System.currentTimeMillis();
-            String result = call(cf, new Object[] {"file:" + input.getAbsolutePath(), getClass().getClassLoader()});
-            logger.debug("The compilation of '" + input + "' took " + (System.currentTimeMillis () - time) + " ms.");
-            return result;
+	}
+	
+	public String compile(File input) throws LessException {
+		try {
+			long time = System.currentTimeMillis();
+			String result = call(cf, new Object[] {"file:" + input.getAbsolutePath(), getClass().getClassLoader()});
+			logger.debug("The compilation of '" + input + "' took " + (System.currentTimeMillis () - time) + " ms.");
+			return result;
 		} catch (Exception e) {
-			throw parseJSException(e);
+			throw parseLessException(e);
 		}
-    }
-    
-    public void compile(File input, File output) throws LessException, IOException {
-    	try {
-	        String content = compile(input);
-	        if (!output.exists()) {
-	            output.createNewFile();
-	        }
-	        BufferedWriter bw = new BufferedWriter(new FileWriter(output));
-	        bw.write(content);
-	        bw.close();
+	}
+	
+	public void compile(File input, File output) throws LessException, IOException {
+		try {
+			String content = compile(input);
+			if (!output.exists()) {
+				output.createNewFile();
+			}
+			BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+			bw.write(content);
+			bw.close();
 		} catch (Exception e) {
-			throw parseJSException(e);
+			throw parseLessException(e);
 		}
-    }
+	}
 
-    private synchronized String call(Function fn, Object[] args) {
+	private synchronized String call(Function fn, Object[] args) {
 		return (String) Context.call(null, fn, scope, scope, args);
-    }
-    
-    private LessException parseJSException(Exception root) throws LessException
-    {
-    	logger.debug("Parsing JS exception", root);
-    	
+	}
+	
+	private LessException parseLessException(Exception root) throws LessException {
+		
+		logger.debug("Parsing LESS Exception", root);
+		
 		if (root instanceof JavaScriptException) {
 			
-			JavaScriptException jse = (JavaScriptException) root;
+			Scriptable value = (Scriptable) ((JavaScriptException) root).getValue();
 			
-			Scriptable val = (Scriptable) jse.getValue();
-			
-			boolean hasName = ScriptableObject.hasProperty((Scriptable) val, "name");
-			boolean hasType = ScriptableObject.hasProperty((Scriptable) val, "type");
+			boolean hasName = ScriptableObject.hasProperty((Scriptable) value, "name");
+			boolean hasType = ScriptableObject.hasProperty((Scriptable) value, "type");
 			
 			if (hasName || hasType) {
 				String errorType = "Error";
 				
-				if(hasName) {
-					String type = (String) ScriptableObject.getProperty(((Scriptable) val), "name");
+				if (hasName) {
+					String type = (String) ScriptableObject.getProperty(((Scriptable) value), "name");
 					if ("ParseError".equals(type)) {
 						errorType = "Parse Error";
-					}
-					else {
+					} else {
 						errorType = type + " Error";
 					}
-				}
-				else if (hasType) {
-					Object prop = ScriptableObject.getProperty(((Scriptable) val), "type");
+				} else if (hasType) {
+					Object prop = ScriptableObject.getProperty(((Scriptable) value), "type");
 					if (prop instanceof String) {
 						errorType = (String) prop + " Error"; 
 					}
 				}
 				
-				String message = (String) ScriptableObject.getProperty(((Scriptable) val), "message");
+				String message = (String) ScriptableObject.getProperty(((Scriptable) value), "message");
 				
 				String filename = "";
-				if (ScriptableObject.hasProperty(val, "filename")) {
-					filename = (String) ScriptableObject.getProperty(((Scriptable) val), "filename"); 
+				if (ScriptableObject.hasProperty(value, "filename")) {
+					filename = (String) ScriptableObject.getProperty(((Scriptable) value), "filename"); 
 				}
 				
 				int line = -1;
-				if (ScriptableObject.hasProperty(val, "line")) {
-					line = ((Double) ScriptableObject.getProperty(((Scriptable) val), "line")).intValue(); 
+				if (ScriptableObject.hasProperty(value, "line")) {
+					line = ((Double) ScriptableObject.getProperty(((Scriptable) value), "line")).intValue(); 
 				}
 				
 				int column = -1;
-				if (ScriptableObject.hasProperty(val, "column")) {
-					column = ((Double) ScriptableObject.getProperty(((Scriptable) val), "column")).intValue();
+				if (ScriptableObject.hasProperty(value, "column")) {
+					column = ((Double) ScriptableObject.getProperty(((Scriptable) value), "column")).intValue();
 				}
 				
 				
 				List<String> extractList = new ArrayList<String>();
-				if (ScriptableObject.hasProperty(val, "extract")) {
-					NativeArray extract = (NativeArray) ScriptableObject.getProperty(((Scriptable) val), "extract");
-					
+				if (ScriptableObject.hasProperty(value, "extract")) {
+					NativeArray extract = (NativeArray) ScriptableObject.getProperty(((Scriptable) value), "extract");
 					for (int i = 0; i < extract.getLength(); i++) {
 						if (extract.get(i, extract) instanceof String) {
-							extractList.add(((String) extract.get(i, extract)).replace('\t', ' '));
+							extractList.add(((String) extract.get(i, extract)).replace("\t", " "));
 						}
 					}
 				}
 				
-				throw LessException.fromLessJsError(errorType, message, filename, line, column, extractList);
+				throw new LessException(message, errorType, filename, line, column, extractList);
 			}
 		}
 		
 		throw new LessException(root);
-    }
-    
+	}
+	
 }
